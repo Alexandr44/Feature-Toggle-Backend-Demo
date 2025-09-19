aeba7ecc7bc6b8eca83bc8e547a5874954347141
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-09-19 06:01:16 +0500

    Added User endpoints

--- src/main/kotlin/com/alexandr44/featuretogglebackenddemo/controller/UserController.kt
+++ src/main/kotlin/com/alexandr44/featuretogglebackenddemo/controller/UserController.kt
@@ -1,12 +1,12 @@
 package com.alexandr44.featuretogglebackenddemo.controller
 
 import com.alexandr44.featuretogglebackenddemo.dto.UserDto
+import com.alexandr44.featuretogglebackenddemo.dto.UserEditDto
 import com.alexandr44.featuretogglebackenddemo.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -18,6 +18,23 @@ class UserController(
     @PreAuthorize("hasRole('ADMIN')")
     fun getAll(): ResponseEntity<List<UserDto>> {
         return ResponseEntity.ok(userService.getAll())
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    fun editUser(
+        @PathVariable id: UUID,
+        @RequestBody userEditDto: UserEditDto
+    ): ResponseEntity<UserDto> {
+        return ResponseEntity.ok(userService.editUser(id, userEditDto))
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    fun delete(
+        @PathVariable id: UUID
+    ): ResponseEntity<UserDto> {
+        return ResponseEntity.ok(userService.deleteUser(id))
     }
 
 }
---
+++ src/main/kotlin/com/alexandr44/featuretogglebackenddemo/dto/UserEditDto.kt
@@ -0,0 +1,10 @@
+package com.alexandr44.featuretogglebackenddemo.dto
+
+import com.alexandr44.featuretogglebackenddemo.enums.UserRole
+
+data class UserEditDto(
+    val username: String?,
+    val password: String?,
+    val role: UserRole?,
+    val isActive: Boolean?
+)
